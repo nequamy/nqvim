6df6477cf13bc7a84db00f4bff92643f38f50790
nequamy <nequamy@gmail.com>
2026-02-24 12:25:23 +0300

    [~] settings in init.lua for performance improvement

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
-vim.g._start_time = vim.fn.reltime()
+vim.loader.enable()
+
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider    = 0
+vim.g.loaded_perl_provider    = 0
+vim.g.loaded_node_provider    = 0
+
+vim.g._start_time             = vim.fn.reltime()
 
 require("config")
 require("plugins")
