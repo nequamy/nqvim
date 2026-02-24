825ead79df6ad4ca5abd5bca23d97b3fe13cee98
nequamy <nequamy@gmail.com>
2026-02-24 12:24:01 +0300

    [~] move fzf settings to settings path

---
+++ lua/settings/fzf.lua
@@ -0,0 +1,33 @@
+local actions = require('fzf-lua.actions')
+
+return {
+	winopts = {
+		backdrop = 85,
+		preview = { default = "bat", delay = 20 }
+	},
+	keymap = {
+		builtin = {
+			["<S-f>"] = "preview-page-down",
+			["<S-b>"] = "preview-page-up",
+			["<S-p>"] = "toggle-preview",
+		},
+		fzf = {
+			["ctrl-a"] = "toggle-all",
+			["ctrl-t"] = "first",
+			["ctrl-g"] = "last",
+			["ctrl-d"] = "half-page-down",
+			["ctrl-u"] = "half-page-up",
+		},
+	},
+	actions = {
+		files = {
+			["ctrl-q"] = actions.file_sel_to_qf,
+			["ctrl-n"] = actions.toggle_ignore,
+			["ctrl-h"] = actions.toggle_hidden,
+			["enter"]  = actions.file_edit_or_qf,
+		},
+	},
+	fzf_opts = {
+		["--highlight-line"] = true,
+	}
+}
